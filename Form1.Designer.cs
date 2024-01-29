b05ae2a98811370c3a20d1f771a25e86f9081b8d
Josh Byrnes <204185+realJoshByrnes@users.noreply.github.com>
2024-01-29 23:49:45 +1100

    Update to current project

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -20,20 +20,28 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+    #region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
-        }
-
-        #endregion
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      SuspendLayout();
+      // 
+      // Form1
+      // 
+      AutoScaleDimensions = new SizeF(7F, 15F);
+      AutoScaleMode = AutoScaleMode.Font;
+      ClientSize = new Size(800, 450);
+      IsMdiContainer = true;
+      Name = "Form1";
+      Text = "Mono Chat Client";
+      Load += Form1_Load;
+      ResumeLayout(false);
     }
+
+    #endregion
+  }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,15 @@
 namespace mono_chat_client
 {
-    public partial class Form1 : Form
+  public partial class Form1 : Form
+  {
+    public Form1()
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+      InitializeComponent();
     }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      
+    }
+  }
 }
