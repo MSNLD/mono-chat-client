013522c64fa55d4cf18d87e5c6d19681837528c1
Josh Byrnes <204185+realJoshByrnes@users.noreply.github.com>
2024-01-30 19:52:29 +1100

    Catch CLASS_NOT_REG exception

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 using AxMSNChat;
+using System.Runtime.InteropServices;
 
 namespace mono_chat_client
 {
@@ -26,14 +27,25 @@ namespace mono_chat_client
         axChatFrame.Dock = DockStyle.Fill;
         Controls.Add(axChatFrame);
       }
-      catch (System.IO.FileNotFoundException) // OCX can be installed, or found locally.
+      catch (System.IO.FileNotFoundException)
       {
+        // MsnChat45.ocx is not found locally (SxS Assembly)
         MessageBox.Show("MsnChat45.ocx is missing.\nThis application will now close.");
+        Close();
+      }
+      catch (COMException ex)
+      {
+        // Class not registered. We shouldn't really hit this if our manifests are doing their job.
+        if (ex.ErrorCode == -2147221164)
+          MessageBox.Show("MsnChat45.ocx is not installed\nThis application will now close.");
+        else
+          MessageBox.Show(ex.Message);
         Close();
       }
       catch (Exception ex)
       {
         MessageBox.Show(ex.Message);
+        Close();
       }
     }
   }
