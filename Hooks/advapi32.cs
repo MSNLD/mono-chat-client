9373b7fdb50b6ced0c57c967315341ee730ad6ec
Josh Byrnes <204185+realJoshByrnes@users.noreply.github.com>
2024-02-05 00:16:44 +1100

    Added Random GateKeeper for MSNChat45

--- Hooks/advapi32.cs
+++ Hooks/advapi32.cs
@@ -1,9 +1,11 @@
 ﻿using EasyHook;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.Text;
 using Windows.Win32;
 using Windows.Win32.Foundation;
 using Windows.Win32.System.Registry;
+using Windows.Win32.System.SystemServices;
 
 namespace mono_chat_client.Hooks
 {
@@ -20,10 +22,20 @@ namespace mono_chat_client.Hooks
 
     public static WIN32_ERROR RegOpenKeyExAHook(HKEY hKey, PCSTR lpSubKey, uint ulOptions, REG_SAM_FLAGS samDesired, HKEY* phkResult)
     {
-      var result = PInvoke.RegOpenKeyExA(hKey, lpSubKey, ulOptions, samDesired, phkResult);
-      var _phkResult = Marshal.PtrToStructure<HKEY>((nint)phkResult);
-      Debug.WriteLine($"RegOpenKeyEx: {{ hKey: {hKey}, lpSubKey: {lpSubKey}, ulOptions: {ulOptions}, samDesired: {samDesired}, phkResult: {_phkResult} }}: {result}");
-      return result;
+      // TODO: It would be better if we could do this per instance of the chat client, and check the GUID used in AxHost.
+      // Currently, we are specifying MSNChat45's GUID explicitly.
+      if (lpSubKey.ToString() == "CLSID\\{F58E1CEF-A068-4C15-BA5E-587CAF3EE8C6}")
+      {
+        // This will cause a random GateKeeper address to be issued on MSNChat45.
+        return WIN32_ERROR.ERROR_FILE_NOT_FOUND;
+      }
+      else
+      {
+        var result = PInvoke.RegOpenKeyExA(hKey, lpSubKey, ulOptions, samDesired, phkResult);
+        var _phkResult = Marshal.PtrToStructure<HKEY>((nint)phkResult);
+        Debug.WriteLine($"RegOpenKeyEx: {{ hKey: {hKey}, lpSubKey: {lpSubKey}, ulOptions: {ulOptions}, samDesired: {samDesired}, phkResult: {_phkResult} }}: {result}");
+        return result;
+      }
     }
   }
 }
