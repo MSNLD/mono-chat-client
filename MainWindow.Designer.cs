da2f08163f1bfa88e81e5dfeb35666434d60a223
Ricardo de Vries <ricardozegt@gmail.com>
2024-02-15 13:39:36 +0100

    chore: Rename Form1 to MainWindow

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace mono_chat_client
 {
-    partial class Form1
+    partial class MainWindow
     {
         /// <summary>
         ///  Required designer variable.
--- MainWindow.cs
+++ MainWindow.cs
@@ -3,9 +3,9 @@ using System.Runtime.InteropServices;
 
 namespace mono_chat_client
 {
-  public partial class Form1 : Form
+  public partial class MainWindow : Form
   {
-    public Form1()
+    public MainWindow()
     {
       InitializeComponent();
     }
